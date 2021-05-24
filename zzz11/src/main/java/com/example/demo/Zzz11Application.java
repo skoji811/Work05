43f582b8363a944be0e9409f9b319bdc05957e27
skoji811 <s.koji811@gmail.com>
2021-05-24 17:42:27 +0900

    first commit

---
+++ zzz11/src/main/java/com/example/demo/Zzz11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zzz11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zzz11Application.class, args);
+	}
+
+}
