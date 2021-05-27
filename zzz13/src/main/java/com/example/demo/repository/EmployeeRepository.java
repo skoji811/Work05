41945e108d4ea9c1c6330ac9e39106d3bca6d98b
skoji811 <s.koji811@gmail.com>
2021-05-27 23:59:45 +0900

    修正

--- zzz13/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ zzz13/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
 
