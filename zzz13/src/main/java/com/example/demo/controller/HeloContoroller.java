86b21a3619412d7d3db673cf350c37d7bc6c26f5
skoji811 <s.koji811@gmail.com>
2021-05-28 17:21:15 +0900

    テスト

--- zzz13/src/main/java/com/example/demo/controller/HeloContoroller.java
+++ zzz13/src/main/java/com/example/demo/controller/HeloContoroller.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller;
 
-public class HeloContoroller {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Controller
+public class HeloContoroller {
+    @Autowired
+    EmployeeRepository empRepository;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        List<Employee> emplist=empRepository.findAll();
+        model.addAttribute("emplist", emplist);
+        return "index";
+    }
 }
