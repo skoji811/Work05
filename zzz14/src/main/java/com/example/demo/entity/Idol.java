f58f2332ca0726101f339babbb7fabf94f3f7041
skoji811 <s.koji811@gmail.com>
2021-05-28 21:26:06 +0900

    getter setter @Data検証

--- zzz14/src/main/java/com/example/demo/entity/Idol.java
+++ zzz14/src/main/java/com/example/demo/entity/Idol.java
@@ -9,14 +9,35 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
+
 @Entity
 @Table(name = "idol")
-@Data
 public class Idol {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String name;
-    @Column(name="age")
+	private String name;
+    @Column(name ="age")
     private Integer age;
+    
+    public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+  
 }
