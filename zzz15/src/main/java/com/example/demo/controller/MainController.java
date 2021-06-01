980959240ba580070468c287da523dedf2662464
skoji811 <s.koji811@gmail.com>
2021-06-01 21:53:29 +0900

    Crud処理

--- zzz15/src/main/java/com/example/demo/controller/MainController.java
+++ zzz15/src/main/java/com/example/demo/controller/MainController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Idol;
@@ -28,15 +30,27 @@ private String insert(@ModelAttribute("formInsert") Idol idol, Model model) {
 repository.saveAndFlush(idol);
 return "redirect:/";
 }
-@RequestMapping("/update")
-public String update(@ModelAttribute("updateInsert")Idol idol,Model model) {
-repository.saveAndFlush(idol);
-return "redirect:/";
+@GetMapping("/update")
+public String update(Model model) {
+	Iterable<Idol> list = repository.findAll();
+	model.addAttribute("data", list);
+	return "update";
 }
-@RequestMapping("/delete")
-public String delete(@ModelAttribute("deleteInsert")Idol idol,Model model) {
-	repository.delete(idol);
-	return "redirect:/";
+@PostMapping("/update")
+public String setUpdate(@ModelAttribute("updateInsert")Idol idol,Model model) {
+	repository.saveAndFlush(idol);
+	return "index";
+	}
+@GetMapping("/delete")
+public String delete(Model model) {
+	Iterable<Idol> list = repository.findAll();
+	model.addAttribute("data", list);
+	return "delete";
+}
 
+@PostMapping("/delete")
+public String setDelete(@ModelAttribute("deleteInsert")Idol idol,Model model) {
+	repository.delete(idol);
+	return "index";
 }
 }
