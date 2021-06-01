a9a34239c069042975fd0f91b28d3db4e76c36f4
skoji811 <s.koji811@gmail.com>
2021-06-01 14:46:57 +0900

    テスト

--- zzz15/src/main/java/com/example/demo/entity/Idol.java
+++ zzz15/src/main/java/com/example/demo/entity/Idol.java
@@ -7,8 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "idol")
+
 public class Idol {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
