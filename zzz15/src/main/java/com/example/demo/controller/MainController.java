798c0bf49bada6f436eeac6b1d84c0913919736e
skoji811 <s.koji811@gmail.com>
2021-06-01 11:55:31 +0900

    Crud処理　修正

---
+++ zzz15/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.Idol;
+import com.example.demo.repository.IdolRepository;
+
+@Controller
+public class MainController {
+
+@Autowired
+IdolRepository repository;
+@RequestMapping("/")
+private String index(Model model) {
+Iterable<Idol> list = repository.findAll();
+model.addAttribute("data", list);
+return "index";
+}
+}
